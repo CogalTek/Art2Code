73aa6e9e5c30dbf096680dfa8b5e68a9523b1c69
Mathieu Rio <mathieurio@Air-de-Mathieu.lan>
2023-10-28 09:56:49 +0200

    Feat: Invite to discord

--- Art2Code/pages/Information.vue
+++ Art2Code/pages/Information.vue
@@ -59,6 +59,8 @@
             </div>
         </div>
 
+        <iframe class="my-5" src="https://discord.com/widget?id=1154411423284330578&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+
     </div>
   </template>
   
